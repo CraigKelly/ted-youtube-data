abdaa68481b0574da6d2123c0fa516f940660235
Craig Kelly <craig.n.kelly@gmail.com>
2017-02-13 13:47:28 -0600

    Tranlating html into transcript text files

--- README.md
+++ README.md
@@ -7,17 +7,19 @@ TODO: where to get the 2 CSV files?
 Everything in this project is build on the expectation that the two CSV files
 `ted_talks_raw.csv`  and `yt_ted_raw.csv` are already present.
 
-You also need [dmk](https://github.com/CraigKelly/dmk) and Python 3. You will also
-need some Python 3 libraries that your system might not have installed by default:
+You also need [dmk](https://github.com/CraigKelly/dmk) and Python 3. You will
+also need some Python 3 libraries that your system might not have installed by
+default:
 
 * requests
 * beautifulsoup4
+* html5lib
 
-To install both of these to your local user location (instead of the global
-system location), you can run:
+To install these to your local user location (instead of the global system
+location), you can run:
 
 ```
-pip3 install --user requests beautifulsoup4
+pip3 install --user requests beautifulsoup4 html5lib
 ```
 
 ## Running
---
+++ xlate_transcripts.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+"""Get transcript links from stdin and write to specified dir."""
+
+# pylama:ignore=E501,D213
+
+import argparse
+import os
+import glob
+
+from bs4 import BeautifulSoup
+
+from common import log
+
+
+def ws(s):
+    """Normalize whitespace."""
+    return ' '.join(s.strip().split())
+
+
+def fmtts(ts):
+    """Given a timestamp in seconds, return a formatted string."""
+    hr, mn, sec = 0.0, 0.0, ts
+    while sec > 60.0:
+        sec -= 60.0
+        mn += 1.0
+    while mn > 60.0:
+        mn -= 60.0
+        hr += 1.0
+    return "{:02.0f}:{:02.0f}:{:07.4f}".format(hr, mn, sec)
+
+
+def lines(html):
+    """Yield (second,text) as found in the HTML."""
+    soup = BeautifulSoup(html, 'html5lib')
+    for d in soup.find_all('span'):
+        cls = d.get('class')
+        if not cls or cls[0] != 'talk-transcript__fragment':
+            continue
+        ts = float(d.get('data-time')) / 1000.0
+        txt = ws(d.get_text())
+        yield (ts, txt)
+
+
+def xlate_one(filename):
+    """Retrieval logic for a single URL."""
+    if not os.path.isfile(filename):
+        raise ValueError("Could not find input file " + filename)
+
+    return list(lines(open(filename).read()))
+
+
+def main():
+    """Entry point."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dir", help="output directory")
+    args = parser.parse_args()
+
+    if not args.dir or not os.path.isdir(args.dir):
+        raise ValueError("No directory specified")
+
+    def outfile(fn):
+        return os.path.join(args.dir, fn)
+
+    for inp in glob.glob(outfile("*.html")):
+        newfn = inp.replace(".html", ".txt")
+        with open(inp.replace(".html", ".txt"), "w") as outp:
+            for ts, txt in xlate_one(inp):
+                ts = fmtts(ts)
+                outp.write("{} {}\n".format(ts, txt))
+        log("Wrote %s", newfn)
+
+
+if __name__ == '__main__':
+    main()
