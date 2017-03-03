622721796b769eac324028c174f63b5dfc2815f9
Craig Kelly <craig.n.kelly@gmail.com>
2017-03-03 15:32:15 -0600

    Updating for Python3 and PEP 8

---
+++ ytscrape/YTCrawl/lib/logger.py
@@ -0,0 +1,61 @@
+"""The logger class supporting the crawler."""
+
+# pylama:ignore=D212,D213,E501
+
+# Author: Honglin Yu <yuhonglin1986@gmail.com>
+# License: BSD 3 clause
+
+
+import time
+
+from os.path import join
+
+
+class Logger(object):
+    """record the crawling status, error and warnings."""
+
+    def __init__(self, outputDir=""):
+        """init."""
+        self._output_dir = outputDir
+        self._log_file_dict = {'log': open(join(self._output_dir, 'log'), 'a+r')}
+
+        # self._mutex_done = threading.Lock()
+        # self._mutex_log = threading.Lock()
+
+        self._done_file = open(join(self._output_dir, 'key.done'), 'a+r')
+
+    def add_log(self, d):
+        """Add a log file."""
+        for i, j in d.items():
+            self._log_file_dict[i] = open(join(self._output_dir, j), 'a+r')
+
+    def get_key_done(self, lfkl):
+        """Get the keys that have been crawled."""
+        r = []
+
+        for i in lfkl:
+            tmp = self._log_file_dict[i]
+            for l in tmp:
+                r.append(eval(l)[1])
+
+        return r + [x.rstrip('\n') for x in self._done_file]
+
+    def log_done(self, k):
+        """Thread safe finalizer for logs."""
+        # self._mutex_done.acquire()
+        self._done_file.write('%s\n' % k)
+        self._done_file.flush()
+        # self._mutex_done.release()
+
+    def log_warn(self, k, m, lfk='log'):
+        """Log message as warning.
+
+        Arguments:
+        - `k` : the key
+        - `m`: the message
+        - `lfk`: log_file_key
+        """
+        # self._mutex_log.acquire()
+        self._log_file_dict[lfk].write(str([time.strftime('%Y_%m_%d_%m_%H_%M'), k, m]) + '\n')
+        self._log_file_dict[lfk].flush()
+        # self._mutex_log.release()
